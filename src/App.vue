09e4d45fba38fab020551a0b87bd76897f6c67e2
bojun1995 <bojun1995@foxmail.com>
2022-02-07 22:07:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() { },
+  mounted() { },
+  methods: {},
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss" type="text/scss" scoped>
+#app {
+  height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>vue2 question</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() { },
+  mounted() { },
+  methods: {},
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss" type="text/scss" scoped></style>
